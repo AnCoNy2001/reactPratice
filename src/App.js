2ba024eaa78038772c8e26c4e09026944542ae58
Ân Bùi <97715641+AnCoNy2001@users.noreply.github.com>
2023-06-04 18:40:45 +0700

    ddd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello Ân đẹp trai</div>;
+  return <div className="App">Hello anh Ân đẹp trai</div>;
 }
 
 export default App;
